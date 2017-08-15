6bc1462f0d5402a76028c4bd1492ed8ef8c998b2
kylearora <kylearora@gmail.com>
2017-08-15 18:55:34 -0500

    Created root page and linked users to individual user pages

---
+++ app.js
@@ -0,0 +1,38 @@
+const data = require ("./data")
+const express = require("express")
+const app = express()
+// const path = require ("path")
+const mustache = require("mustache-express")
+app.engine('mustache', mustache())
+app.set('views', './views')
+app.set('view engine', 'mustache')
+app.use( express.static('public'))
+
+app.get("/", function(request, response){
+  const title = "Express is super fun!"
+  const dataInfo = data.users
+  response.render('index', {
+    title: title,
+    data: dataInfo
+  })
+})
+
+app.get('/users/:id', function(request, response){
+
+  const robot = parseInt(request.params.id)
+  let profile = false;
+  for (var i = 0; i < data.users.length; i++) {
+    if (data.users[i].id === robot){
+      profile = data.users[i]
+    }
+  }
+  const dataInfo = data.users
+  response.render("profile", {
+    profile: profile,
+    data: dataInfo
+  })
+})
+
+app.listen(3000, function(){
+  console.log("Express started on port 3000")
+})
